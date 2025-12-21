f151146a13b0869d0162548b3bddf0f30891360b
Divyansh <divyanshvijay@gmail.com>
2025-12-21 20:39:16 +0530

    add

---
+++ list/new.py
@@ -0,0 +1,16 @@
+# # for i in range(1,31):
+# #     if i ==20:
+# #         break
+# #     print(i)
+
+# for i in range(1,21):
+#     if i== 10:
+#         continue
+#     print(i)
+
+# for i in range(3):
+#     print(i)
+
+# for i in range(10,0,-2):
+#     print(i)
+
